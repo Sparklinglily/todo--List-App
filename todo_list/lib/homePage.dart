8bb37c5f5a8c4d9c66e680f7210b31570c322df3
Sparklinglily <carlaejemeh@gmail.com>
2023-11-02 12:51:18 +0100

    webview

--- todo_list/lib/homePage.dart
+++ todo_list/lib/homePage.dart
@@ -1,56 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+            physics: null,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 25.0),
+              child: Column(
+                //mainAxisAlignment: MainAxisAlignment.center,
+                //crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Card(
+                    margin: EdgeInsets.only(left: 24, right: 24),
+                    elevation: 3,
+                    //  shape: CircleBorder(side: BorderSide(width: 4)),
+                    child: Image.asset(
+                      'images/carla.jpg',
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 7,
+                  ),
+                  Text(
+                    'Name: Carla Ejemeh Inya-Agha',
+                    style: TextStyle(fontSize: 21),
+                  ),
+                  SizedBox(
+                    height: 7,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => WebViewPage()));
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(255, 235, 159, 194),
+                          borderRadius: BorderRadius.circular(24)),
+                      child: Text('GitHub Profile'),
+                    ),
+                  )
+                ],
+              ),
+            )),
+      ),
+    );
+  }
 }
 
-class _HomePageState extends State<HomePage> {
-  final TextEditingController textController = TextEditingController();
-  String typedText = '';
+class WebViewPage extends StatefulWidget {
+  const WebViewPage({super.key});
 
   @override
-  void dispose() {
-    textController.dispose();
-    super.dispose();
+  State<WebViewPage> createState() => _WebViewPageState();
+}
+
+class _WebViewPageState extends State<WebViewPage> {
+  late final WebViewController webViewController;
+  bool isLoading = true;
+  final key = UniqueKey();
+
+  @override
+  void initState() {
+    webViewController = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progess) {
+            setState(() {
+              isLoading = false;
+            });
+          },
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+        ),
+      )
+      ..loadRequest(Uri.parse('https://github.com/Sparklinglily'));
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 20.0, left: 15, right: 15),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 200,
-                width: 250,
-                child: Card(
-                  child: Text(typedText),
-                  elevation: 1,
-                ),
-              ),
-              SizedBox(
-                height: 8,
-              ),
-              TextField(
-                controller: textController,
-                onChanged: (text) {
-                  setState(() {
-                    typedText = text;
-                  });
-                },
-                decoration: InputDecoration(
-                    border:
-                        OutlineInputBorder(borderSide: BorderSide(width: 2))),
-              )
-            ],
-          ),
+      appBar: AppBar(),
+      body: Stack(children: [
+        WebViewWidget(
+          controller: webViewController,
+          key: key,
         ),
-      ),
+        if (isLoading)
+          Center(
+            child: const CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation(Colors.amber),
+            ),
+          )
+      ]),
     );
   }
 }
